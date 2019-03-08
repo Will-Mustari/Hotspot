baa6665a0874bcb1eaeb2c8424be24ca28414904
evankalaw <e.c.kalaw@gmail.com>
2019-03-08 17:03:17 -0600

    empty xcode

---
+++ HotSpot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotSpot
+//
+//  Created by Evan Kalaw on 3/8/19.
+//  Copyright © 2019 CS506 HotSpot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
