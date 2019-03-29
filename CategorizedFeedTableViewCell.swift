c4c8c7c50f09159e1b2c92e174ef7c7773eb31d6
Ben Gordon <bgordon5@wisc.edu>
2019-03-28 19:28:03 -0500

    Adding CategorizedFeed functionality

---
+++ CategorizedFeedTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategorizedFeedTableViewCell.swift
+//  HotSpot
+//
+//  Created by Benjamin J Gordon on 3/28/19.
+//  Copyright © 2019 CS506 HotSpot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategorizedFeedTableViewCell: UITableViewCell{
+    @IBOutlet weak var barName: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var overallRating: UITextView!
+    @IBOutlet weak var vibeRating: UILabel!
+}
--- HotSpot/Controllers/CategorizedFeedViewController.swift
+++ HotSpot/Controllers/CategorizedFeedViewController.swift
@@ -7,10 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
-class CategorizedFeedViewController: UIViewController {
+struct bar {
+    let barName : String!
+    let address : String!
+}
 
+class CategorizedFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var bars:[BarInformation] = []
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bars.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CategorizedFeedTableViewCell else {
+            fatalError("Could not dequeue a cell")
+        }
+        let bar = bars[indexPath.row]
+        cell.address.text = bar.address
+        cell.barName.text = bar.uniqueBarNameID
+        
+        return cell
+    }
+    func getBars(){
+        
+    }
+    @IBOutlet weak var barTableView: UITableView!
     override func viewDidLoad() {
+        //        let databaseRef = FIRDatabase.database().reference()
+//        databaseRef.child("Bars").queryOrderedByKey().observedEventType(.ChildAdded, withBlock: { snapshot in
+//            
+//            let barName = snapshot.value!["barName"]
+//            let address = snapshot.value!["address"]
+//
+//            self.posts.insert(postStruct(barName: barName, address: address), atIndex: 0)
+//        })
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
