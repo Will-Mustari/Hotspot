6a03e92554a82b82d3809240d5aa0f1a4dcfa90d
joepineda747 <josephcgpineda@gmail.com>
2019-04-08 21:06:08 -0500

    updated BarSelect

--- HotSpot/Controllers/BarSelectViewController.swift
+++ HotSpot/Controllers/BarSelectViewController.swift
@@ -17,13 +17,12 @@ class BarSelectViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var reviewTable: UITableView!
     
     
-    let barName = "Sconnie Bar"
-    let address = "101 Regent St, Madison WI 53714"
+    var barName = ""
+    let address = ""
     var ratings:[ReviewInformation] = []
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         
         //Initialize database
         let database = Firestore.firestore()
@@ -32,11 +31,16 @@ class BarSelectViewController: UIViewController, UITableViewDelegate, UITableVie
         barNameLabel.text = barName
         addressLabel.text = address
         
-        loadReviews()
+        loadReviews { (loadedRatings) in
+            self.ratings = loadedRatings
+            self.reviewTable.reloadData()
+        }
+        
         reviewTable.delegate = self
         reviewTable.dataSource = self
         reviewTable.reloadData()
         print(ratings)
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
@@ -44,8 +48,10 @@ class BarSelectViewController: UIViewController, UITableViewDelegate, UITableVie
         return ratings.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell") as! ReviewsTableViewCell
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell") as? ReviewsTableViewCell else {
+            fatalError("Could not dequeue a cell")
+        }
         let rating = ratings[indexPath.row]
         
         cell.tableTextLabel.text = rating.review
@@ -54,26 +60,35 @@ class BarSelectViewController: UIViewController, UITableViewDelegate, UITableVie
     }
 
 
-    func loadReviews() {
+    func loadReviews(completion: @escaping ([ReviewInformation]) -> Void){
+        
         db.collection("Ratings").whereField("barName", isEqualTo: barName)
             .getDocuments() { (querySnapshot, err) in
                 if let err = err {
                     print("Error getting documents: \(err)")
                 } else {
+                    var rateArray:[ReviewInformation] = []
                     for document in querySnapshot!.documents {
-                        var newRating = ReviewInformation.init(review: "",rating: 0,vibes: [],barName: "",userId: "")
                         
+                        let rating = document.data()["rating"] as! Double
+                        let review = "\"" + (document.data()["review"] as! String) + "\""
+                        let barName = document.data()["barName"] as! String
+                        let userID = document.data()["userID"] as! String
+                        let vibes = document.data()["vibes"] as! [String]
                         
-                        newRating.rating = document.data()["rating"] as! Double
-                        newRating.review = document.data()["review"] as! String
-                        newRating.barName = document.data()["barName"] as! String
-                        newRating.userId = document.data()["userID"] as! String
-                        newRating.vibes = document.data()["vibes"] as! [String]
+                        let newRating = ReviewInformation.init(review: review,rating: rating,vibes: vibes,barName: barName,userId: userID)
+                        print(newRating.barName)
+                        print(newRating.review)
+
                         
-                        self.ratings.append(newRating)
+                        rateArray.append(newRating)
                     }
+                    
+                    completion(rateArray)
                 }
         }
+        
+
     }
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
