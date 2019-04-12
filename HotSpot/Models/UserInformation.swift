775549efccee42382e70bfe265e6bbee9dc6c1ca
evankalaw <e.c.kalaw@gmail.com>
2019-04-11 21:18:05 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/Will-Mustari/Hotspot""
    This reverts commit 05adbef198ecfa8444a16d3a39bf730f025fbffc.

--- HotSpot/Models/UserInformation.swift
+++ HotSpot/Models/UserInformation.swift
@@ -22,7 +22,7 @@ struct UserInformation {
         ]){error in
             if error != nil{
                 //handle error
-                print(error?.localizedDescription)
+                print(error?.localizedDescription as Any)
             }else{
                 print("Document saved correctly")
             }
